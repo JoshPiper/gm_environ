f4e7ad76f13568275cf59c2313fb0b555cc29797
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-12-30 19:24:30 +0000

    style: remove unused error macro.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(c_unwind)]
 
+use std::env;
 #[cfg(feature = "gmcl")]
 use gmod::gmcl::override_stdout;
 use gmod::lua::{State};
@@ -8,41 +9,54 @@ use gmod::lua_function;
 #[macro_use] extern crate gmod;
 
 static MOD_NAME: &str = "environ";
-macro_rules! err {
-    () => {format!("{} had an error.", MOD_NAME)};
-    ($arg:literal) => {format!("{} was unable to {}", MOD_NAME, $arg)};
-    ($arg:literal, $err:literal) => {format!("{} was unable to {}: {:?}", MOD_NAME, $arg, $err)};
-}
 
 unsafe fn error(lua: State, err: &str){
     lua.error(err);
 }
 
-unsafe fn arg_err(lua: State, pos: i16, exp: &str, real: &str){
-    error(lua, format!("{}: Bad Argument in position #{}, expected {} got {}", MOD_NAME, pos, exp, real).as_str());
-}
+// unsafe fn arg_err(lua: State, pos: i16, exp: &str, real: &str){
+//     error(lua, format!("{}: Bad Argument in position #{}, expected {} got {}", MOD_NAME, pos, exp, real).as_str());
+// }
 
 #[lua_function]
 unsafe fn index(lua: State) -> i32 {
-    let arg_type = lua.get_type(-1);
-    if (arg_type != "string"){
-        arg_err(lua, 1, "string", arg_type);
+    let str_idx = lua.check_string(2);
+    let env_var = env::var(str_idx.as_ref());
+
+    if env_var.is_err() {
+        println!("failed to read: {}", env_var.err().unwrap());
+        lua.push_nil();
+    } else {
+        println!("{} -> {}: {}", MOD_NAME, str_idx, env_var.clone().unwrap());
+        lua.push_string(env_var.unwrap().as_str())
     }
 
-    let arg = lua.get_string(-1);
-    let environ_key: str = match arg {
-        None => {
-            error(lua, err!("fetch argument string").as_str());
-            ""
+    1
+}
+
+#[lua_function]
+unsafe fn get_path(lua: State) -> i32 {
+    let env_var = env::var("PATH");
+    match env_var {
+        Ok(val) => {
+            let val = val.as_str();
+            let split = val.split(PATH_SEP).collect::<Vec<&str>>();
+            lua.create_table(split.len() as i32, 0);
+            let mut i = 0;
+            for s in split {
+                if s != "" {
+                    i += 1;
+                    lua.push_string(s);
+                    lua.raw_seti(-2, i);
+                }
+            }
         },
-        Some(arg_str) => {
-            *arg_str
+        Err(err) => {
+            println!("{} -> {}: {}", MOD_NAME, "PATH", err);
+            lua.new_table();
         }
-    };
-
-    println!("{}: {}", arg_type, environ_key);
-
-    0
+    }
+    1
 }
 
 #[lua_function]
@@ -55,12 +69,12 @@ unsafe fn newindex(lua: State) -> i32 {
 unsafe fn gmod13_open(lua: State) -> i32 {
     macro_rules! export_lua_function {
         ($name:ident) => {
-            // _G.sysinfo.$name
+            // _G.environ.$name
             lua.push_function($name);
             lua.set_field(-2, concat!(stringify!($name), "\0").as_ptr() as *const i8);
         };
         ($func:ident, $name:literal) => {
-            // _G.sysinfo.$name
+            // _G.environ.$name
             lua.push_function($func);
             lua.set_field(-2, lua_string!($name));
         }
@@ -72,7 +86,7 @@ unsafe fn gmod13_open(lua: State) -> i32 {
 
     // Create _G.environ
     lua.create_table(0, 1);
-    export_lua_function!(newindex, "__ignore");
+    export_lua_function!(get_path);
 
     // Create _G.environ metatable
     lua.create_table(0, 1);
@@ -89,6 +103,6 @@ unsafe fn gmod13_open(lua: State) -> i32 {
 
 #[gmod13_close]
 fn gmod13_close(_lua: State) -> i32 {
-    println!("Goodbye from binary module!");
     0
 }
+
